35ff14217156ed3b45aba9408c8d7c6a8dfa5caa
Mansi <mansipatel241@gmail.com>
2022-11-17 09:22:14 +0530

    Day 4 : Checkout Flow

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -1,7 +1,82 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
+import { Container, Typography, Toolbar, Box, Paper, Stepper, Step,StepLabel, Button, Avatar, useTheme, TextField, createTheme, ThemeProvider } from '@mui/material'
+import AddressForm from '../components/AddressForm';
+import PaymentsForm from '../components/PaymentsForm';
+import ReviewForm from '../components/ReviewForm';
+import { useDispatch } from 'react-redux';
+import { clearCart } from '../feature/cart-slice';
+import { clearCheckoutInformation } from '../feature/checkout-slice';
+import {Link} from 'react-router-dom'
 
+const steps = ["Shipping Address", "Payment Details", "Review Order"];
+
+function getStepContent(activeStep) {
+    switch(activeStep) {
+        case 0:
+            return <AddressForm />
+        case 1:
+            return <PaymentsForm/>
+        case 2:
+            return <ReviewForm />
+        default:
+            throw new Error("Unknown Step")
+    }
+}
 export default function Checkout() {
+
+  const [activeStep, setActiveStep] = useState(0);
+  const dispatch = useDispatch();
+
+  useEffect(()=>{
+    if(activeStep == steps.length) {
+        dispatch(clearCart());
+        dispatch(clearCheckoutInformation());
+    }
+  }, [activeStep])
+
+  function handleNext() {
+    setActiveStep(activeStep + 1);
+  }
+
+  function handleBack() {
+    setActiveStep(activeStep - 1);
+  }
+
   return (
-    <div>Checkout</div>
+    <Container component="section" maxWidth="lg" sx={{mb: 4}}>
+        <Paper variant="outlined" sx={{my:{xs:3, md:6}, p:{xs:2, md:3}}}>
+            <Typography component="h1" variant="h4" align="center">
+                Checkout
+            </Typography>
+            <Stepper activeStep={activeStep} sx={{
+                pt:3,pb:5
+            }}>
+                {steps.map((label)=>(<Step key={label}>
+                    <StepLabel>{label}</StepLabel>
+                </Step>))}
+            </Stepper>
+            {
+                activeStep === steps.length? <>
+                <Typography variant="h5" gutterBottom>Thank you for your order</Typography>
+                <Typography>Your order number is #D1G42634. We have emailed you the details regarding your order confirmation.</Typography>
+                <Link to="/">Shop More</Link>
+                </>:<>
+                {getStepContent(activeStep)}
+                <Box sx={{
+                    display:"flex",
+                    justifyContent:"flex-end"
+                }}>
+                    {activeStep !==0 && (
+                    <Button onClick={handleBack} variant="contained" sx={{
+                        mt:3, ml:1
+                    }}>Back</Button>) }
+                    <Button onClick={handleNext}  variant="contained" sx={{
+                        mt:3, ml:1
+                    }}>{activeStep === steps.length-1? "Place Order" : "Next"}</Button>
+                </Box>
+                </>
+            }
+        </Paper>
+    </Container>
   )
 }
--- src/store.js
+++ src/store.js
@@ -1,13 +1,15 @@
 import { configureStore } from "@reduxjs/toolkit";
 import cartReducer from "./feature/cart-slice"
 import categoriesReducer from "./feature/categories-slice";
+import checkoutReducer from "./feature/checkout-slice";
 import productsReducer from "./feature/products-slice";
 
 export const store = configureStore({
     reducer : {
         cart: cartReducer,
         products: productsReducer,
-        categories: categoriesReducer
+        categories: categoriesReducer,
+        checkout: checkoutReducer
     }
 });
 
@@ -23,3 +25,12 @@ export const store = configureStore({
 //reducers only work with synchronous operation, reducers are synchronous by default
 //asynchronous action : thunk dispatches an async action, based on result it dispatches another action
 //use log monitor in the bottom of redux extension to see why promise got rejected.
+
+
+
+//Learnt : 
+//Material UI
+//Filtering and Searching Functionality
+//User management using Firebase Authentication: Signup, Login 
+//Protected Route
+//Redux Toolkit - create store, action, reducers
